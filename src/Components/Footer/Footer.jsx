866598e2a0585988e5151b70d2d1075d5a044623
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-08-19 15:52:27 +0300

    Page one

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -458,6 +458,39 @@ c49 -25 54 -68 15 -114 -43 -52 -104 -39 -104 22 0 31 37 107 52 107 5 0 22
           />
         </div>
       </div>
+      <div className="footer-bottom flex space-between">
+        <p className="bluish"> © 2022 Amos. All rights reversed</p>
+        <div className="flex">
+          <svg
+            version="1.0"
+            xmlns="http://www.w3.org/2000/svg"
+            width="48.000000pt"
+            height="48.000000pt"
+            viewBox="0 0 48.000000 48.000000"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <metadata>
+              Created by potrace 1.16, written by Peter Selinger 2001-2019
+            </metadata>
+            <g
+              transform="translate(0.000000,48.000000) scale(0.100000,-0.100000)"
+              fill="#fff"
+              stroke="none"
+            >
+              <path
+                d="M56 384 c-23 -23 -23 -265 0 -288 23 -24 345 -24 368 0 23 23 23 265
+0 288 -23 24 -345 24 -368 0z m322 -20 c42 -12 24 -38 -58 -82 l-80 -43 -80
+43 c-79 43 -101 71 -62 81 26 7 254 8 280 1z m-136 -164 c9 0 51 19 93 41 l77
+42 -3 -76 c-3 -42 -8 -79 -12 -84 -11 -10 -303 -10 -314 0 -4 5 -9 42 -11 84
+l-4 75 79 -41 c43 -23 86 -41 95 -41z"
+              />
+            </g>
+          </svg>
+          <a href="https://github.com/AmohPrince" className="git">
+            git
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
